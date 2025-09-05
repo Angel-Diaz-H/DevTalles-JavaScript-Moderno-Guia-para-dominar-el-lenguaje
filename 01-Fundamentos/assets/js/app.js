e15c3054c6009fa193d58a519a9e84e55a71bbfb
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-05 08:40:06 -0600

    Orden y lugar de las importaciones

---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,44 @@
+// console.log("Hola mundo!");
+
+/* Alt + Shift + A */
+// A mí me funcionó usar:
+// Ctrl + K Ctrl + C.
+
+
+/* Así se crean las variables y la constante, más utilizadas en JavaScript. */
+// Iniciar una variable.
+/*
+let a = 10; //
+var b = 20; // Antes se inicializaba así.
+const c = 30;
+*/
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+/*
+console.warn(x)
+console.error(x)
+*/
+
+/*
+console.log('a', a);
+console.log('b', b);
+console.log('c', c);
+*/
+
+// Estos son objectos
+// console.log('%c Mis variables', 'color:blue; font-weight:bold');
+// console.log({a});
+// console.log({b});
+// console.log({c});
+
+// console.table({a, b, c, d, x});
+
+// Esto no es buena práctica.
+var miNombre = 'Angel';
