83b95c5a797e59fe8c3b85ea63d048a199b159ca
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-05 09:13:00 -0600

    global y window

---
+++ 01-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,12 @@
+// alert('Hola mundo.');
+
+// let nombre = prompt('Cuál es tu nombre?', 'Sin nombre');
+// console.log('****' + nombre + '****');
+
+// const seleccion = confirm('¿Estas seguro de borrar esto?');
+
+// console.log(seleccion);
+
+
+// global existe en Node, pero en el navegador no. 
+console.log(global)
