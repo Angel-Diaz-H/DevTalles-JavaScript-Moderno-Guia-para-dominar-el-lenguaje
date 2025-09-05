f9bd2f738d27a3e0cb260a7dab72ac80211731a3
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-05 11:31:57 -0600

    Introducción general a los tipos primitivos

---
+++ 01-Fundamentos/assets/js/primitivos.js
@@ -0,0 +1,36 @@
+let nombre = 'Peter Parker';
+console.log(nombre);
+
+// Se recomienda las comillas simples, pero depende del contexto.
+nombre = 'Ben Parker';
+console.log(nombre);
+
+// Otras maneras de declararlas.
+nombre = "Tía May";
+nombre = `Tía May`;
+
+console.log(typeof(nombre));
+console.log(typeof(nombre));
+
+nombre = 123;
+console.log(typeof(nombre));
+
+let esMarvel = true;
+console.log(typeof(esMarvel));
+
+let edad = 33;
+console.log(typeof(edad));
+
+edad = 33.001;
+console.log(typeof(edad));
+
+let superPoder;
+console.log(typeof(superPoder));
+
+let soyNull = null;
+console.log(typeof(soyNull));
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(symbol1 === symbol2);
