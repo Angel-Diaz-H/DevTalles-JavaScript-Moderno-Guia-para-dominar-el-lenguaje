d571da61551072c208aaba16af0edbecc77b05cd
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-09 19:19:49 -0600

    Pro tip: Asignación con operadores

--- 01-Fundamentos/assets/js/logica-boolena.js
+++ 01-Fundamentos/assets/js/logica-boolena.js
@@ -34,3 +34,22 @@ console.log(true || false); // true
 // No ejecutó el de false porque el primer valor ya dio verdadero.
 console.log(regresaTrue() || regresaFalse());
 console.log('4 condiciones', true || true || true || false); // true
+
+
+console.warn('Asignaciones');
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = true && 'Hola mundo' && 150; // qué?
+const a2 = 'Hola' && 'mundo' && soyFalso && true; // qué
+const a3 = soyFalso || 'Ya no soy falso'; // qué
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo' || true; // qué
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy falso de nuevo' || true; // qué
+console.log({a1, a2, a3, a4, a5});
+
+if (regresaFalse() && regresaTrue() && (true || true || true || false)) {
+    console.log('Hacer algo');
+} else {
+    console.log('Hacer otra cosa');
+};
