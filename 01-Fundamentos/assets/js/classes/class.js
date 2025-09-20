043ddcb1fe79ed042af7d48b68b5eea78f1bfec8
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-19 20:10:43 -0600

    sets y gets

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -1,16 +1,38 @@
 // UpperCamelCase para clases.
 class Persona {
+    // Propiedades y métodos estáticos
+    // Son opcionales declararlas aquí.
     nombre = '';
     codigo = '';
     frase = '';
+    comida = '';
 
     // Constructor es un método que se va a ejecutar cuando se crea una instancia
     constructor(nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase') {
+        
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
     };
 
+    // set
+    // Es para asignar un valor.
+    // Se suele recibir solo un parámetro.
+    // No pueden tener el mismo nombre que la propiedad.
+    // La gente suele declararlas así _comida.
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    // get
+    // Es para obtener un valor.
+    // Se suele recibir solo un parámetro.
+    // No pueden tener el mismo nombre que la propiedad.
+    get getComidaFavorita() {
+        return `La comida favorita de ${this.nombre} es: ${this.comida}`;
+    }
+
+    // Métodos.
     quienSoy(){
         console.log(`Soy ${this.nombre} - ${this.codigo} - ${this.frase}`);
     }
@@ -23,8 +45,11 @@ class Persona {
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'I am SpiderMan');
 const ironman = new Persona('Tony Stark', 'Ironman', 'I am Ironman');
-console.log(spiderman);
 console.log(ironman);
 
 spiderman.miFrase();
 ironman.miFrase();
+
+spiderman.setComidaFavorita = 'El pay de cereza de la tía May';
+console.log(spiderman);
+console.log(spiderman.getComidaFavorita)
