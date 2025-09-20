9d6a08be743bb52d000ccf6b52fdc720f52797e7
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-20 14:33:18 -0600

    Crear deck - Módulos por defecto e individuales

--- 04-blackjack-vite/src/blackjack/index.js
+++ 04-blackjack-vite/src/blackjack/index.js
@@ -25,7 +25,7 @@ const divCartasComputadora = document.querySelector('#computadora-cartas');
 
 const puntosHTML = document.querySelectorAll('small');
 
-crearDeck();
+deck = crearDeck(tipos, especiales);
 
 // Esta función me permite tomar una carta
 const pedirCarta = () => {
