83861b81120888969651a98776b87dd81757716b
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-20 16:52:06 -0600

    Tarea - Refactorizar pedirCarta y valorCarta.

--- 04-blackjack-vite/src/blackjack/index.js
+++ 04-blackjack-vite/src/blackjack/index.js
@@ -1,5 +1,7 @@
 import _ from 'underscore'
 import { crearDeck } from './usecases/crear-deck.js';
+import { pedirCarta } from './usecases/pedir-carta.js';
+import { valorCarta } from './usecases/valor-carta.js';
 
 /**
  * 2C = Two of Clubs
@@ -27,30 +29,11 @@ const puntosHTML = document.querySelectorAll('small');
 
 deck = crearDeck(tipos, especiales);
 
-// Esta función me permite tomar una carta
-const pedirCarta = () => {
-
-    if ( deck.length === 0 ) {
-        throw 'No hay cartas en el deck';
-    }
-    const carta = deck.pop();
-    return carta;
-}
-
-// pedirCarta();
-const valorCarta = ( carta ) => {
-
-    const valor = carta.substring(0, carta.length - 1);
-    return ( isNaN( valor ) ) ? 
-            ( valor === 'A' ) ? 11 : 10
-            : valor * 1;
-}
-
 // turno de la computadora
 const turnoComputadora = ( puntosMinimos ) => {
 
     do {
-        const carta = pedirCarta();
+        const carta = pedirCarta(deck);
 
         puntosComputadora = puntosComputadora + valorCarta( carta );
         puntosHTML[1].innerText = puntosComputadora;
@@ -80,12 +63,10 @@ const turnoComputadora = ( puntosMinimos ) => {
     }, 100 );
 }
 
-
-
 // Eventos
 btnPedir.addEventListener('click', () => {
 
-    const carta = pedirCarta();
+    const carta = pedirCarta(deck);
     
     puntosJugador = puntosJugador + valorCarta( carta );
     puntosHTML[0].innerText = puntosJugador;
@@ -111,7 +92,6 @@ btnPedir.addEventListener('click', () => {
 
 });
 
-
 btnDetener.addEventListener('click', () => {
     btnPedir.disabled   = true;
     btnDetener.disabled = true;
@@ -123,7 +103,7 @@ btnNuevo.addEventListener('click', () => {
 
     console.clear();
     deck = [];
-    deck = crearDeck();
+    deck = crearDeck(tipos, especiales);
 
     puntosJugador     = 0;
     puntosComputadora = 0;
