d52fa0cd809107b20c36f300b9c3b1dd39c489c8
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-20 20:36:56 -0600

    Vite.config new path

--- 04-blackjack-vite/vite.config.js
+++ 04-blackjack-vite/vite.config.js
@@ -1,6 +1,6 @@
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-    base: '/DevTalles-JavaScript-Moderno-Guia-para-dominar-el-lenguaje',
+    base: '/DevTalles-JavaScript-Moderno-Guia-para-dominar-el-lenguaje/',
     plugins: []
 });
