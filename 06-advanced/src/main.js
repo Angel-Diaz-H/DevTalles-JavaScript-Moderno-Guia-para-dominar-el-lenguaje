aed36981f85e96dc252a555fcde44dee6a4f4cd4
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-30 13:34:23 -0600

    Variables de entorno

--- 06-advanced/src/main.js
+++ 06-advanced/src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
+import { enviromentsComponent } from './concepts/01-enviroments'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -17,5 +18,6 @@ document.querySelector('#app').innerHTML = `
 
   </div>
 `
+const element = document.querySelector('.card');
 
-console.log('Hello mundo!');
+enviromentsComponent(element);
