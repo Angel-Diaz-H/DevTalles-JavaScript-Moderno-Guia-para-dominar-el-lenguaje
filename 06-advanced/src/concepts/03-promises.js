b8a05d8bfa2a197939ab99991935bcce3c86bebe
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-01 10:06:38 -0600

    Promise All

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -26,16 +26,35 @@ export const promiseComponent = (element) => {
     const id1 = '5d86371f97c29d020f1e1f6d';
     const id2 = '5d86371fd55e2e2a30fe1cc3';
 
-    let hero1;
+    // Mandar las promesas como listas de manera simultanea.
+    // Con que una promesa da error, pasa por el catch y no se ejecuta ninguna promesa.
+    Promise.all([
+        findHero(id1),
+        findHero(id2)
+    ])
+    .then(([hero1, hero2]) => renderTwoHero(hero1, hero2))
+    .catch(renderError);
 
-    findHero(id1)
-        .then((hero1) =>{
-            findHero(id2)
-                .then(hero2 => {
-                    renderTwoHero(hero1, hero2);
-                });
-        })
-        .catch(renderError);
+    //! Forma 2.
+    // let hero1, hero2;
+    // findHero(id1)
+    //     .then(hero =>{
+    //         hero1 = hero;
+    //         return findHero(id2);
+    //     }).then(hero => {
+    //         hero2 = hero;
+    //         renderTwoHero(hero1, hero2);
+    //     })
+
+    //! Forma 1.
+    // findHero(id1)
+    //     .then((hero1) =>{
+    //         findHero(id2)
+    //             .then(hero2 => {
+    //                 renderTwoHero(hero1, hero2);
+    //             });
+    //     })
+    //     .catch(renderError);
 };
 
 /**
