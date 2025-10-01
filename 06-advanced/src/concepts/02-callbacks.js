5d5b97305bf95595cc1bea077c2e517fcd94363c
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-30 18:25:17 -0600

    Manejo de errores en los callbacks.

--- 06-advanced/src/concepts/02-callbacks.js
+++ 06-advanced/src/concepts/02-callbacks.js
@@ -6,18 +6,29 @@ import { heroes } from "../data/heroes";
  */
 export const callbacksComponent = (element) => {
     const id = '5d86371f25a058e5b1c8a65e';
-    findHero(id, (hero) => {
-        element.innerHTML = hero.name;
+    findHero(id, (error, hero) => {
+        // element.innerHTML = hero?.name || 'No hay héroe';
+
+        if(error) {
+            element.innerHTML = error;
+            return;
+        }
+        element.innerHTML = hero?.name;
     });
 };
 
 /**
  * 
  * @param {String} id 
- * @param {(hero: Object) => void} callback 
+ * @param {(error: String|Null, hero: Object) => void} callback 
  */
 const findHero = (id, callback) => {
     const hero = heroes.find(heroe => heroe.id === id);
 
-    callback(hero);
+    if(!hero) {
+        callback(`Hero with id ${id} not found`);
+        return; // undefined;
+    }
+
+    callback(null, hero);
 };
