6208601be91b596c2adb312e5950e269b20fb82b
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-30 19:59:26 -0600

    Promesas.

--- 06-advanced/src/main.js
+++ 06-advanced/src/main.js
@@ -3,6 +3,7 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { enviromentsComponent } from './concepts/01-enviroments'
 import { callbacksComponent } from './concepts/02-callbacks'
+import { promiseComponent } from './concepts/03-promises'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -22,4 +23,5 @@ document.querySelector('#app').innerHTML = `
 const element = document.querySelector('.card');
 
 // enviromentsComponent(element);
-callbacksComponent(element);
+// callbacksComponent(element);
+promiseComponent(element);
