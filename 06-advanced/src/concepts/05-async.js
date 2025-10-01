246466a48f621b2a663e77db053860d25c2f8beb
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-01 10:47:09 -0600

    Manejo de errores en funciones asíncronas

--- 06-advanced/src/concepts/05-async.js
+++ 06-advanced/src/concepts/05-async.js
@@ -5,10 +5,12 @@ import { heroes } from "../data/heroes";
  * @param {HTMLDivElement} element 
  */
 export const asyncComponent = (element) => {
-    const id1 = '5d86371fd55e2e2da30fe1cc4';
+    const id1 = '5d86371fd55e2e2a30fe1cc4';
+    console.log('Inicio de componente');
     findHero(id1)
         .then(name => element.innerHTML = name)
         .catch(error => element.innerHTML = error)
+    console.log('Fin de componente');
 };
 
 /**
@@ -18,5 +20,7 @@ export const asyncComponent = (element) => {
  */
 const findHero = async (id) => {
     const hero = heroes.find(hero => hero.id === id);
+    if (!hero)
+        throw `Hero with id ${id} not found`;
     return hero?.name;
 }
