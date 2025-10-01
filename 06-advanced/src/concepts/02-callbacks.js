fe16ac4a40b9778afa5c3a90ddc8233d0d0e16d9
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-09-30 19:44:59 -0600

    Callbacks hell.

--- 06-advanced/src/concepts/02-callbacks.js
+++ 06-advanced/src/concepts/02-callbacks.js
@@ -5,15 +5,24 @@ import { heroes } from "../data/heroes";
  * @param {HTMLDivElement} element 
  */
 export const callbacksComponent = (element) => {
-    const id = '5d86371f25a058e5b1c8a65e';
-    findHero(id, (error, hero) => {
+    const id1 = '5d86371f25a058e5b1c8a65e';
+    const id2 = '5d86371f1efebc31def272e2';
+
+    findHero(id1, (error, hero1) => {
         // element.innerHTML = hero?.name || 'No hay héroe';
 
         if(error) {
             element.innerHTML = error;
             return;
         }
-        element.innerHTML = hero?.name;
+
+        findHero(id2, (error, hero2) =>{
+            if(error) {
+                element.innerHTML = error;
+                return;
+            }   
+            element.innerHTML = `${hero1.name} / ${hero2.name}`;
+        });
     });
 };
 
