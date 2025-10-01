220c07c548839bb39ca857d5fc577cf6fee09f64
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-01 09:22:41 -0600

    Utilizar una promesa.

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -5,7 +5,23 @@ import { heroes } from "../data/heroes";
  * @param {HTMLDivElement} element 
  */
 export const promiseComponent = (element) => {
-    
+    const renderHero = (hero) => {
+        element.innerHTML = hero.name;
+    };
+
+    const renderError = (error) => {
+        element.innerHTML = `
+        <h1>Error:</h1>
+        <h3>${error}</h3>
+        `;
+    };
+
+    const id1 = '5d86371f97c29d020f1e1f6d';
+    const id2 = '5d86371fd55e2e2a30fe1cc3';
+
+    findHero(id1)
+        .then(renderHero)
+        .catch(error => renderError(error));
 };
 
 /**
