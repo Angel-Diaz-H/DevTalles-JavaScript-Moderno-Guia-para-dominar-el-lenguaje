5fdb2cd9305163da7aaceccb74365cfce726c43c
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-03 08:11:41 -0600

    Errores en async await

--- 06-advanced/src/concepts/06-async-await.js
+++ 06-advanced/src/concepts/06-async-await.js
@@ -8,10 +8,15 @@ export const asyncAwaitComponent = async(element) => {
     const id1 = '5d86371fd55e2e2a30fe1cc3';
     const id2 = '5d86371fd55e2e2a30fe1ccb1';
 
-    const hero1 = await findHero(id1);
-    const hero2 = await findHero(id2);
+    try{
+        const hero1 = await findHero(id1);
+        const hero2 = await findHero(id2);
+    
+        element.innerHTML = `${hero1.name} / ${hero2.name}`;
+    } catch(error) {
+        element.innerHTML = error;
+    }
 
-    element.innerHTML = `${hero1.name} / ${hero2.name}`;
 };
 
 const findHero = async(id) => {
