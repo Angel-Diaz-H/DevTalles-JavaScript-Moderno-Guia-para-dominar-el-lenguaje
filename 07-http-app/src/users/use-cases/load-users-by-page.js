c85874aff466f3f954ff9483eff6e774cbb8086f
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-05 17:10:31 -0600

    Cargar información del usuario al modal.

--- 07-http-app/src/users/use-cases/load-users-by-page.js
+++ 07-http-app/src/users/use-cases/load-users-by-page.js
@@ -1,9 +1,10 @@
 import { localhostUserToModel } from "../mappers/localhost-user.mapper";
+import { User } from "../models/user";
 
 /**
  * 
  * @param {Number} page 
- * @returns {Promise<users[]>}
+ * @returns {Promise<User[]>}
  */
 export const loadUsersByPage = async(page = 1) =>{
     const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
