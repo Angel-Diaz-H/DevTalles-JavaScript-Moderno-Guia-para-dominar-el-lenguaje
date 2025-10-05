7685176667955b56eb34cddcb8de8cba2566f0de
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-10-04 21:33:59 -0600

    Mappers

--- 07-http-app/src/users/use-cases/load-users-by-page.js
+++ 07-http-app/src/users/use-cases/load-users-by-page.js
@@ -1,12 +1,14 @@
+import { localhostUserToModel } from "../mappers/localhost-user.mapper";
+
 /**
  * 
  * @param {Number} page 
- * @returns 
+ * @returns {Promise<users[]>}
  */
 export const loadUsersByPage = async(page = 1) =>{
     const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
     const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
-    return data;
+    const {data} = await res.json();
+    const users = data.map(localhostUserToModel);
+    return users;
 };
